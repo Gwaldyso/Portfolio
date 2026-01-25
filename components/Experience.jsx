84993b813c7b5f42e6c11ebafe2523d113eef21c
Gwaldys Otounga <otoungaarmelle43@gmail.com>
2026-01-25 16:43:18 +0100

    Update portfolio content and design

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -147,7 +147,7 @@ export default function Experience() {
             Carrières
           </span>
           <h2 className="text-3xl sm:text-4xl font-bold text-gray-900">
-            Experiences proffessionnelles
+            Expériences proffessionnelles
           </h2>
         </motion.div>
 
